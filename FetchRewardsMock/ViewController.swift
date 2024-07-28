435fd37b473504617995a07d7c58d35a1f7cbee3
Edvin Lellhame <edvinlel@gmail.com>
2024-07-28 14:10:36 -0700

    Initial Commit

---
+++ FetchRewardsMock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FetchRewardsMock
+//
+//  Created by Edvin on 7/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
